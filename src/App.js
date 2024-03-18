48b634dba414ada8adfb967f9d97947748bad19f
Maxi Zanabria <maxi.zanabria@hotmail.com>
2024-03-18 20:28:50 -0300

    primer modificacion

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Usuario } from './components/Usuario';
+function App() {
+
+
+  return (
+    <div className='App'>
+      <h1> . Kushklan .</h1>
+      <hr></hr>
+      <Usuario />
+      <hr></hr>      
+      <Usuario />
+      <hr></hr>
+      <Usuario />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Usuario.js
@@ -0,0 +1,8 @@
+export const Usuario = () => {
+    return <div> 
+                <h1>Nombre : maxx</h1>
+                <p>Edad: 28</p>
+                <p>Nacionalidad: argentino </p>
+            </div>
+} 
+
